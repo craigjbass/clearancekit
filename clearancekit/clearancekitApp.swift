67bd73b8706c2a38ce85f413911618b346e31849
Craig Bass <craig@madetech.com>
2026-01-26 23:39:50 +0000

    Initial Commit

---
+++ clearancekit/clearancekitApp.swift
@@ -0,0 +1,17 @@
+//
+//  clearancekitApp.swift
+//  clearancekit
+//
+//  Created by Craig J. Bass on 26/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct clearancekitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
